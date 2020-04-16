5e32bb49b1265fc50f00581ccce996610ec8ce86
gf <1191738602@qq.com>
2020-04-16 17:31:18 +0800

    springCloud 删除多余的引用

--- providertwos/src/main/java/com/providertwos/util/Util.java
+++ providertwos/src/main/java/com/providertwos/util/Util.java
@@ -5,8 +5,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.sun.corba.se.spi.ior.ObjectKey;
-import org.aspectj.apache.bcel.classfile.Module;
 
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
